4d0630dc5ea88028433ebb9e6cd7de10f0d36992
odeat <dua2003@live.nl>
2025-04-30 19:54:33 +0200

    Update settings to disable GitHub Copilot for all file types; refactor particle and spring initialization in setup function.

--- sketch.js
+++ sketch.js
@@ -17,60 +17,15 @@ function setup() {
 
     let bounds = new Rect(0, 0, width - 10, height - 10); // borders of the world
     physics.setWorldBounds(bounds);
-
-// Particles (16 in an oval shape)
-particles.push(new Particle(300, 100));  // 0
-particles.push(new Particle(345, 115));  // 1
-particles.push(new Particle(380, 150));  // 2
-particles.push(new Particle(395, 200));  // 3
-particles.push(new Particle(400, 300));  // 4
-particles.push(new Particle(395, 400));  // 5
-particles.push(new Particle(380, 450));  // 6
-particles.push(new Particle(345, 485));  // 7
-particles.push(new Particle(300, 500));  // 8
-particles.push(new Particle(255, 485));  // 9
-particles.push(new Particle(220, 450));  //10
-particles.push(new Particle(205, 400));  //11
-particles.push(new Particle(200, 300));  //12
-particles.push(new Particle(205, 200));  //13
-particles.push(new Particle(220, 150));  //14
-particles.push(new Particle(255, 115));  //15
-
-// Springs (connect all particles in an oval loop)
-springs.push(new Spring(particles[0], particles[1], 50, 0.1));
-springs.push(new Spring(particles[1], particles[2], 50, 0.1));
-springs.push(new Spring(particles[2], particles[3], 50, 0.1));
-springs.push(new Spring(particles[3], particles[4], 50, 0.1));
-springs.push(new Spring(particles[4], particles[5], 50, 0.1));
-springs.push(new Spring(particles[5], particles[6], 50, 0.1));
-springs.push(new Spring(particles[6], particles[7], 50, 0.1));
-springs.push(new Spring(particles[7], particles[8], 50, 0.1));
-springs.push(new Spring(particles[8], particles[9], 50, 0.1));
-springs.push(new Spring(particles[9], particles[10], 50, 0.1));
-springs.push(new Spring(particles[10], particles[11], 50, 0.1));
-springs.push(new Spring(particles[11], particles[12], 50, 0.1));
-springs.push(new Spring(particles[12], particles[13], 50, 0.1));
-springs.push(new Spring(particles[13], particles[14], 50, 0.1));
-springs.push(new Spring(particles[14], particles[15], 50, 0.1));
-springs.push(new Spring(particles[15], particles[0], 50, 0.1));
-
-springs.push(new Spring(particles[0], particles[4], 100, 0.03));
-springs.push(new Spring(particles[8], particles[12], 100, 0.03));
-
-springs.push(new Spring(particles[0], particles[8], 80, 0.05));
-springs.push(new Spring(particles[4], particles[12], 80, 0.05));
-springs.push(new Spring(particles[2], particles[10], 80, 0.05));
-springs.push(new Spring(particles[6], particles[14], 80, 0.05));
-springs.push(new Spring(particles[3], particles[11], 80, 0.05));
-springs.push(new Spring(particles[5], particles[13], 80, 0.05));
-springs.push(new Spring(particles[1], particles[9], 80, 0.05));
-springs.push(new Spring(particles[7], particles[15], 80, 0.05));
-
  
+    // springs & particle forloops down here:
+    let start = createVector(100, 0)
+    let particleCount = 8;
+    let draaihoek = PI * 2 / particleCount
 
-    // Add springs to the physics simulation
-    for (let spring of springs) {
-        physics.addSpring(spring);
+    for (let i = 0; i < particleCount; i++) {
+        particles.push(new Particle(start.x + width / 2, start.y + height / 2))
+        start = p5.Vector.rotate(start, draaihoek);
     }
 }
   
